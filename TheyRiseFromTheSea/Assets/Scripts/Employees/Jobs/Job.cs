82300a507d65c9f2f6be4b12f630050adcda845c
cesarrac <cesarrc@gmail.com>
2016-04-08 16:53:39 -0400

    Job Queue - prototyping

---
+++ TheyRiseFromTheSea/Assets/Scripts/Employees/Jobs/Job.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Job {
+
+    // An Employee requires a Tile Type and the Transform of the game Object located at 
+    // the tile where the job is.
+    // A Job requires a Tile Type and a Transform target
+
+    TileData.Types job_tileType;
+    public TileData.Types Job_TileType { get { return job_tileType; } }
+
+    Transform job_target;
+    public Transform Job_Target { get { return job_target; } }
+
+    public Job (TileData.Types tileType, Transform target)
+    {
+        job_tileType = tileType;
+        job_target = target;
+    }
+}
---
+++ TheyRiseFromTheSea/Assets/Scripts/Employees/Jobs/JobRequestManager.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System;
+
+public class JobRequestManager : MonoBehaviour {
+
+    static JobRequestManager instance;
+
+    Queue<Job> jobQueue = new Queue<Job>();
+
+    void Awake()
+    {
+        instance = this;
+    }
+}
